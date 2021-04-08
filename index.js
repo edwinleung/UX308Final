48c0dfea9b3658de9f8337755e1918944ac738b4
Edwin Leung <edwinleung310@gmail.com>
2021-04-08 13:42:39 -0400

    updated price for paypal

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ module.exports = class extends Page {
             <p>${oEntity.full_description}</p>
             <p>${oEntity.price}</p>
             <form action="https://desolate-cliffs-46309.herokuapp.com/payment" method="post">
-            <input type ="hidden" name ="price" value ="21" />
+            <input type ="hidden" name ="price" value ="${oEntity.price}" />
             <input type="hidden" name="title" value="${oEntity.title}" />
             <input type="tel" placeholder="Enter your number" name="telephone"/>
             <button type="submit">Order now</button>
